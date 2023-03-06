3d4b86e0a73b492cfb464dabbcbc0a6e21b24711
leander <leander97@gmx.de>
2023-03-06 14:14:43 +0900

    implemented data.py

---
+++ data.py
@@ -0,0 +1,164 @@
+""" Utilities for data loading
+
+General layout of data arrays is [..., zs, smooth, bin]
+where ...  depends on the type of data
+"""
+
+
+import numpy as np
+
+from settings import S
+
+# some data layout constants
+ROOT = '/scratch/gpfs/lthiele/HSC_Y1_Nbody_sims'
+NFID = 2268
+NCOS = 100
+NCOS_RND = 50
+TNG_N_PER_POINT = 5
+NSYST = {
+         'stats_fiducial': NFID,
+         'photoz/frankenz': 200,
+         'photoz/mizuki': 200,
+         'baryons/hydro': 1998//TNG_N_PER_POINT,
+         'baryons/dark': 1998//TNG_N_PER_POINT,
+         'nom': NFID,
+         'real': 1,
+        }
+NSMOOTH_ALL = 8
+NBINS_ALL = {'pdf': 19, 'ps': 14, }
+NZS = len(S['zs'])
+
+FIELDS = {
+          'wide12h': 14.20,
+          'hectomap': 12.26,
+          'vvds': 21.80,
+          'xmm': 32.20,
+          'gama09h': 32.80,
+          'gama15h': 23.73,
+         }
+TOT_AREA = sum(FIELDS.values())
+
+USE_STATS = list(filter(lambda s: s in S, ['pdf', 'ps', ]))
+
+# each entry here is a set of bins to delete, for various reasons
+DELETE_BINS = {
+               k: set(range(S[k]['high_cut'])) \
+                  | set(range(NBINS_ALL[k] - S[k]['low_cut'], NBINS_ALL[k])) \
+                  | {S[k]['delete'], } if k=='pdf' else set() \
+               for k in USE_STATS
+              }
+
+DELETE_SMOOTH = {
+                 k: set(range(NSMOOTH_ALL)) - ( set(S[k]['smooth']) if k=='pdf' else {0, } ) \
+                 for k in USE_STATS
+                }
+
+
+def transf (x, k) :
+    """ possible non-linear transformation, execute only *after* cutting """
+    if S[k]['rebin'] not in [None, 1, ] :
+        x = x.reshape(*x.shape[:-1], -1, S[k]['rebin']).sum(axis=-1)
+
+    if k == 'ps': 
+        x *= 3e9
+    elif S[k]['log'] :
+        # avoid NaN, the chosen number is the minimum non-zero entry
+        x = 13 + np.log(np.maximum(x, 1.2347965399050656e-06))
+    else :
+        x *= 3e1
+
+    return x
+
+
+def cut (x, k) :
+    """ smoothing scale and bin cutting """
+    x = np.delete(x, tuple(DELETE_SMOOTH[k]), axis=-2)
+    x = np.delete(x, tuple(DELETE_BINS[k]), axis=-1)
+    return x
+
+
+def _get_fiducial_impl (k, systematic='stats_fiducial') :
+    
+    out = np.zeros((NSYST[systematic], NZS, NSMOOTH_ALL, NSMOOTH_ALL, NBINS_ALL[k]))
+
+    for ii, zs_idx in enumerate(S['zs']) :
+        
+        # usual case (not kappaTNG)
+        if not systematic or 'baryons' not in systematic :
+            for field_name, field_area in FIELDS.items() :
+                fname = f'{ROOT}/{systematic}/'\
+                        f'{k if k=="pdf" else "power spectrum"}/{k if k=="pdf" else "clee"}_'\
+                        f'{f"z{zs_idx}" if zs_idx else "singlez"}_all_smooths_{field_name}'\
+                        f'{"" if k!="pdf" or not S[k]["unitstd"] or systematic=="real" else "_stdmap"}.npy'
+                f = np.load(fname)
+                if len(out) == 1 ; # special case for reals :
+                    f = f.reshape(1, -1)
+                if k == 'pdf' :
+                    # TODO read this again
+                    out[:, ii, ...] += (field_area / TOT_AREA) \
+                                       * (f / np.mean(np.sum(f, axis=-1), axis=0)[None, :, None])
+                else :
+                    out[:, ii, ...] += (field_area / total_area) * f
+
+        # kappaTNG case
+        else :
+            
+            for jj in range(len(out)) :
+                for kk in range(TNG_N_PER_POINT) :
+                    sim_idx = kk*nsims + jj # some non-consecutive thing to wash out potential correlations
+                    fname = f'{ROOT}/{systematic}/'
+                            f'{f"{k}{"" if not S[k]["unitstd"] else "_unitstd"}" if k=="pdf" else "power_spectrum"}/'\
+                            f'{k if k=="pdf" else "clee"}_{f"z{zs_idx}" if zs_idx else "singlez"}_'\
+                            f'nsim_{sim_idx+1}.npy'
+                    f = np.load(fname)
+                    # TODO read this again
+                    out[jj, ii, ...] += (1/TNG_N_PER_POINT) * (f / np.sum(f, axis=1)[:, None])
+
+    # postprocessing
+    out = cut(out, k)
+    out = transf(out, k)
+    return out.reshape(*out.shape[:1], -1)
+
+
+def _get_cosmo_varied_impl (k, corrected=None) :
+
+    out = np.zeros((NCOS, NCOS_RND, NZS, NSMOOTH_ALL, NBINS_ALL))
+
+    for cc in range(NCOS) :
+        for ii, zs_idx in enumerate(S['zs']) :
+            for field_name, field_area in FIELDS.items() :
+                fname = f'{ROOT}/stats_{f"{corrected}_" if corrected else ""}cosmo_varied/'\
+                        f'{k if k=="pdf" else "power_spectrum"}/model{cc+1}/{k if k=="pdf" else "clee"}'\
+                        f'_{f"z{zs_idx}" if zs_idx else "singlez"}_all_smooths_{field_name}_'\
+                        f'{"" if k!="pdf" or not S[k]["unitstd"] else "_stdmap"}.npy'
+                f = np.load(fname)
+                if k == 'pdf' :
+                    # TODO read this again
+                    out[cc, :, ii, ...] += (field_area / TOT_AREA) \
+                                           * (f / np.mean(np.sum(f, axis=-1), axis=0)[None, :, None])
+                else :
+                    out[cc, :, ii, ...] += (field_area / TOT_AREA) * f
+
+    # postprocessing
+    out = cut(out, k)
+    out = transf(out, k)
+    return out.reshape(*out.shape[:2], -1)
+
+
+def _get_helper (fct, *args, **kwargs) :
+    return np.concatenate([fct(k, *args, **kwargs) for k in USE_STATS], axis=-1)
+
+
+def get_fiducial (systematic='stats_fiducial', cache={}) :
+    if systematic not in cache :
+        cache[systematic] = _get_helper(_get_fiducial_impl, systematic=systematic)
+    return cache[systematic].copy()
+
+
+def get_cosmo_varied (corrected=None, cache={})
+    if corrected not in cache :
+        cache[corrected] = _get_helper(_get_cosmo_varied_impl, corrected=corrected)
+    _, Om, s8 = np.loadtxt(f'{ROOT}/stats_{f"{corrected}_" if corrected else ""}cosmo_varied/'\
+                           f'omegam_sigma8_design3.dat', unpack=True)
+    S8 = s8 * np.sqrt(Om / 0.3)
+    return S8, Om, cache[corrected].copy()
---
+++ settings.py
@@ -0,0 +1,32 @@
+# smoothing scales are
+# 0 --  1 arcmin
+# 1 --  2 arcmin
+# 2 --  5 arcmin
+# 3 --  7 arcmin
+# 4 --  8 arcmin
+# 5 -- 10 arcmin
+# 6 -- 15 arcmin
+# 7 -- 25 arcmin
+
+S = {
+     # source redshift bins, 0 is single-z
+     'zs': [0, 1, 2, 3, 4, ],
+
+     # pdf settings, if not included we do not use pdf
+     'pdf': {
+             'unitstd': True,
+             'log': True,
+             'smooth': [2, 5, ]
+             'rebin': 1,
+             'low_cut': 3,
+             'high_cut': 0,
+             'delete': 9, # delete one bin to prevent ill-conditioned covariance from sum constraint
+            },
+     
+     # power spectrum settings, if not included we do not use power spectrum
+     'ps': {
+            'rebin': 1,
+            'low_cut': 4,
+            'high_cut': 6,
+           },
+    }
