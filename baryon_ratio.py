17762439bb6a6e00d475fee8756895f93fc88591
leander <leander97@gmx.de>
2023-03-09 16:46:58 +0900

    wrote a quick script to compute the baryon ratio

---
+++ baryon_ratio.py
@@ -0,0 +1,39 @@
+import os
+import os.path
+from glob import glob
+
+import numpy as np
+
+ROOT = '/scratch/gpfs/lthiele/HSC_Y1_Nbody_sims'
+
+for stat in ['pdf_stdmap', 'pdf', 'ps', ] :
+    
+    print(f'stat = {stat}')
+    
+    for zstr in ['singlez', 'z1', 'z2', 'z3', 'z4', ] :
+
+        print(f'\tz = {zstr}')
+
+        pattern = f'{"pdf" if stat.startswith("pdf") else "power_spectrum"}'
+        pattern = f'{pattern}/{"pdf" if stat.startswith("pdf") else "clee"}_{zstr}'
+        outfile = pattern
+        pattern = f'{pattern}_nsim_[0-9]*'
+        if 'stdmap' in stat :
+            pattern = f'{pattern}_stdmap'
+            outfile = f'{outfile}_stdmap'
+        pattern = f'{pattern}.npy'
+        outfile = f'{ROOT}/ratio_baryon/{outfile}.npy'
+
+        os.makedirs(os.path.dirname(outfile), exist_ok=True)
+        
+        tng_dark_fnames = glob(f'{ROOT}/kappaTNG/dark/{pattern}')
+        tng_hydro_fnames = glob(f'{ROOT}/kappaTNG/hydro/{pattern}')
+        print(f'Found {len(tng_dark_fnames)} dark files and {len(tng_hydro_fnames)} hydro files')
+        assert len(tng_hydro_fnames) == len(tng_dark_fnames)
+
+        tng_dark_data = np.stack([np.load(fname) for fname in tng_dark_fnames])
+        tng_hydro_data = np.stack([np.load(fname) for fname in tng_hydro_fnames])
+
+        ratio = np.mean(tng_hydro_data, axis=0) / np.mean(tng_dark_data, axis=0)
+
+        np.save(outfile, ratio)
