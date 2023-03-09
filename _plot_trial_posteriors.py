49741a6eb22abb1570b784747dbef8875d56a909
Leander Thiele <lthiele@princeton.edu>
2023-03-08 22:54:08 -0500

    posterior plotting works, but maybe this plot is too complicated for the paper

--- _plot_trial_posteriors.py
+++ _plot_trial_posteriors.py
@@ -11,8 +11,8 @@ from data import Data
 from _plot_stat_runs import *
 from _plot_style import *
 
-NROWS = 3
-NCOLS = 3
+NROWS = 2
+NCOLS = 2
 
 NAX = NROWS * NCOLS
 
@@ -53,9 +53,9 @@ for run_hash in stat_runs.keys() :
 avail_indices = set.intersection(*map(set, all_avail_indices))
 assert len(avail_indices) >= NAX
 
-use_indices = np.random.default_rng().choice(avail_indices, size=NAX, replace=False)
+use_indices = np.random.default_rng().choice(list(avail_indices), size=NAX, replace=False)
 
-fig, ax = plt.subplots(nrows=NROWS+1, ncols=NCOLS, figsize=(20,20),
+fig, ax = plt.subplots(nrows=NROWS, ncols=NCOLS, figsize=(3*NROWS,3*NCOLS),
                        gridspec_kw=dict(hspace=0, wspace=0))
 ax = ax.flatten()
 gridspec = ax[0].get_subplotspec().get_gridspec()
@@ -78,24 +78,30 @@ for ii, (a, idx) in enumerate(zip(ax, use_indices)) :
     cosmo_idx = idx//Data.NSEEDS['cosmo_varied']
     assert cosmo_idx in allowed_cosmo_indices
     true_S8, true_Om = theta_sims[cosmo_idx]
+    new_ax[0, 0].axvline(true_S8, color=black)
     new_ax[1, 0].axvline(true_S8, color=black)
     new_ax[1, 0].axhline(true_Om, color=black)
+    new_ax[1, 1].axvline(true_Om, color=black)
 
     new_ax[0, 1].text(0, 1, S8_text(true_S8), color=black, **S8_text_kwargs(new_ax[0, 1]))
 
     for ii, (run_hash, run_info) in enumerate(stat_runs.items()) :
         chain_fname = f'{ROOT}/cosmo_varied_{run_hash}/chain_{idx}.npz'
         with np.load(chain_fname) as f :
-            chain = f['chain']
+            chain = f['chain'].reshape(-1, 2)
             _true_theta = f['true_theta']
         assert all(close(t1, t2) for t1, t2 in zip(_true_theta, [true_S8, true_Om]))
 
         corner.corner(chain, labels=['$S_8$', '$\Omega_m$', ],
                       range=[(0.45, 1.05), (0.20, 0.40)],
                       color=default_colors[ii],
-                      fig=subfig)
+                      fig=subfig,
+                      plot_datapoints=False, plot_density=False, no_fill_contours=True,
+                      levels=1 - np.exp(-0.5 * np.array([1])**2)
+                     )
         
-        new_ax[0, 1].text(0, 1-0.1*ii, S8_text(np.mean(chain[:,0]), np.std(chain[:,0])),
+        new_ax[0, 1].text(0, 1-0.12*(ii+1), S8_text(np.mean(chain[:,0]), np.std(chain[:,0])),
                           color=default_colors[ii], **S8_text_kwargs(new_ax[0, 1]))
+
 
 savefig(fig, 'trial_posteriors')
