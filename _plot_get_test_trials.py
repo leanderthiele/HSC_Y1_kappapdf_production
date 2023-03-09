7b8ed36fa8ed20707f0e7dc03fdb9376be8ebeb1
leander <leander97@gmx.de>
2023-03-09 13:01:34 +0900

    small refactoring to have reusable get_test_trials for other plotting

---
+++ _plot_get_test_trials.py
@@ -0,0 +1,47 @@
+from glob import glob
+import re
+
+import numpy as np
+
+from data import Data
+
+ROOT = '/scratch/gpfs/lthiele/hsc_chains'
+
+# only consider runs with these true values
+restrict = {
+            'S8': (0.75, 0.85),
+            'Om': (0.25, 0.35),
+           }
+
+def GetTestTrials (run_hashes, N) :
+    """ return N random trial indices that fall into restrict and where we have chains in all runs """
+    
+    data = Data()
+    theta_sims = data.get_cosmo('cosmo_varied')
+    allowed_cosmo_indices = []
+    in_interval = lambda x, t: t[0] <= x <= t[1]
+    for ii, theta in enumerate(theta_sims) :
+        if all(in_interval(t, r) for t, r in zip(theta, restrict.values())) :
+            allowed_cosmo_indices.append(ii)
+
+    # get the runs that we have available
+    pattern = re.compile('chain_([0-9]*).npz')
+    all_avail_indices = []
+    all_avail_cosmo_indices = []
+    for run_hash in run_hashes :
+        chain_fnames = glob(f'{ROOT}/cosmo_varied_{run_hash}/chain_[0-9]*.npz')
+        avail_indices = [int(pattern.search(chain_fname)[1]) for chain_fname in chain_fnames]
+        avail_cosmo_indices = [idx//Data.NSEEDS['cosmo_varied'] for idx in avail_indices]
+        # filter by our restriction
+        avail_indices, avail_cosmo_indices = zip(*((idx, cosmo_idx) \
+                                                   for idx, cosmo_idx in zip(avail_indices, avail_cosmo_indices) \
+                                                   if cosmo_idx in allowed_cosmo_indices))
+        all_avail_indices.append(avail_indices)
+        all_avail_cosmo_indices.append(avail_cosmo_indices)
+
+    # first try to get exactly the same runs, maybe we have enough
+    # if we have about a thougsand runs this is usually enough
+    avail_indices = set.intersection(*map(set, all_avail_indices))
+    assert len(avail_indices) >= N
+
+    return np.random.default_rng().choice(list(avail_indices), size=N, replace=False)
--- _plot_trial_posteriors.py
+++ _plot_trial_posteriors.py
@@ -1,7 +1,3 @@
-from glob import glob
-import re
-from itertools import cycle
-
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -10,6 +6,7 @@ import corner
 from data import Data
 from _plot_stat_runs import *
 from _plot_style import *
+from _plot_get_test_trials import GetTestTrials
 
 NROWS = 2
 NCOLS = 2
@@ -18,49 +15,17 @@ NAX = NROWS * NCOLS
 
 ROOT = '/scratch/gpfs/lthiele/hsc_chains'
 
-# only consider runs with these true values
-restrict = {
-            'S8': (0.75, 0.85),
-            'Om': (0.25, 0.35),
-           }
-
-# get the cosmology indices that fall into restrict
+# do some selection
 data = Data()
 theta_sims = data.get_cosmo('cosmo_varied')
-allowed_cosmo_indices = []
-in_interval = lambda x, t: t[0] <= x <= t[1]
-for ii, theta in enumerate(theta_sims) :
-    if all(in_interval(t, r) for t, r in zip(theta, restrict.values())) :
-        allowed_cosmo_indices.append(ii)
-
-# get the runs that we have available
-pattern = re.compile('chain_([0-9]*).npz')
-all_avail_indices = []
-all_avail_cosmo_indices = []
-for run_hash in stat_runs.keys() :
-    chain_fnames = glob(f'{ROOT}/cosmo_varied_{run_hash}/chain_[0-9]*.npz')
-    avail_indices = [int(pattern.search(chain_fname)[1]) for chain_fname in chain_fnames]
-    avail_cosmo_indices = [idx//Data.NSEEDS['cosmo_varied'] for idx in avail_indices]
-    # filter by our restriction
-    avail_indices, avail_cosmo_indices = zip(*((idx, cosmo_idx) \
-                                               for idx, cosmo_idx in zip(avail_indices, avail_cosmo_indices) \
-                                               if cosmo_idx in allowed_cosmo_indices))
-    all_avail_indices.append(avail_indices)
-    all_avail_cosmo_indices.append(avail_cosmo_indices)
-
-# first try to get exactly the same runs, maybe we have enough
-# if we have about a thougsand runs this is usually enough
-avail_indices = set.intersection(*map(set, all_avail_indices))
-assert len(avail_indices) >= NAX
-
-use_indices = np.random.default_rng().choice(list(avail_indices), size=NAX, replace=False)
+use_indices = GetTestTrials(stat_runs.keys(), NAX)
 
 fig, ax = plt.subplots(nrows=NROWS, ncols=NCOLS, figsize=(3*NROWS,3*NCOLS),
                        gridspec_kw=dict(hspace=0, wspace=0))
 ax = ax.flatten()
 gridspec = ax[0].get_subplotspec().get_gridspec()
 
-# works...
+# some utilities used below
 close = lambda x, y : abs(x-y)<1e-5
 S8_text = lambda avg, std=None : f'$S_8 = {avg:.2f}\pm{std:.2f}$' if std is not None \
                                  else f'$S_8^* = {avg:.2f}$'
@@ -100,7 +65,7 @@ for ii, (a, idx) in enumerate(zip(ax, use_indices)) :
                       levels=1 - np.exp(-0.5 * np.array([1])**2)
                      )
         
-        new_ax[0, 1].text(0, 1-0.12*(ii+1), S8_text(np.mean(chain[:,0]), np.std(chain[:,0])),
+        new_ax[0, 1].text(0, 1-0.14*(ii+1), S8_text(np.mean(chain[:,0]), np.std(chain[:,0])),
                           color=default_colors[ii], **S8_text_kwargs(new_ax[0, 1]))
 
 
