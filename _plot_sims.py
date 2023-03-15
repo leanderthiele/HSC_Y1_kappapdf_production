2f177a1499969abfb2e9f0392bc1f1928058963b
leander <leander97@gmx.de>
2023-03-15 15:47:50 +0900

    added quick script to plot the simulations

---
+++ _plot_sims.py
@@ -0,0 +1,22 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+from data import Data
+from _plot_style import *
+
+S8, Om = Data().get_cosmo('cosmo_varied').T
+
+prior = { 'S8': (0.45, 1.05), 'Om': (0.20, 0.40), }
+
+fig, ax = plt.subplots(figsize=(5,5))
+
+ax.plot(Om, S8)
+rect = plt.Rectangle( (prior['Om'][0], prior['S8'][0]),
+                     prior['Om'][1]-prior['Om'][0], prior['S8'][1]-prior['S8'][0],
+                     fill=False, edgecolor=black)
+ax.add_patch(rect)
+
+ax.set_xlabel('$\Omega_m$')
+ax.set_ylabel('$S_8$')
+
+savefig(fig, 'sims')
