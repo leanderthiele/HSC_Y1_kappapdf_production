31bd827689fdf6f9f9bcf6ed529d06ba85fafe81
Leander Thiele <lthiele@princeton.edu>
2023-03-21 10:25:21 -0400

    found better way to understand catastrophes, no reason to be worried

--- _plot_catastrophes.py
+++ _plot_catastrophes.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 
 import numpy as np
 from matplotlib import pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 from sklearn.neighbors import KernelDensity
 
@@ -11,7 +12,8 @@ from settings import CHAIN_ROOT
 from data import Data
 from _plot_style import *
 
-run_hash = '9d56790a0f55a6885899ec32284b91bd'
+# run_hash = '9d56790a0f55a6885899ec32284b91bd'
+run_hash = '22927c646f516731cb39e41daab9e6e5'
 
 delta = 0.02 # this is the window we consider
 
@@ -36,14 +38,22 @@ ranks = ranks[select]
 
 # select for reasonable S8
 data = Data()
-true_S8 = data.get_cosmo('cosmo_varied')[:, 0]
+sims_S8 = data.get_cosmo('cosmo_varied')[:, 0]
+sims_Om = data.get_cosmo('cosmo_varied')[:, 1]
 nseeds = data.get_nseeds('cosmo_varied')
-selector = lambda x: 0.75<x<0.85
-select = np.array([selector(true_S8[idx // nseeds]) for idx in indices], dtype=bool)
+select_S8 = (0.5, 1.0)
+selector = lambda x: select_S8[0]<x<select_S8[1]
+select = np.array([selector(sims_S8[idx // nseeds]) for idx in indices], dtype=bool)
 indices = indices[select]
 ranks = ranks[select]
 
-print(f'Have {len(indices)} candidates')
+print(f'Have {len(indices)} candidates, should be ~{1500*(2*delta)*(select_S8[1]-select_S8[0])/0.5:.1f}')
+
+true_Om = np.array([sims_Om[idx // nseeds] for idx in indices])
+true_S8 = np.array([sims_S8[idx // nseeds] for idx in indices])
+_, where, counts = np.unique(true_Om, return_index=True, return_counts=True)
+print(true_Om[where])
+print(counts)
 
 rng = np.random.default_rng()
 p = min(( 20/len(indices), 1 ))
@@ -59,6 +69,27 @@ prior = {
 fig, ax = plt.subplots(nrows=2, figsize=(10, 10))
 ax_S8 = ax[0]
 ax_Om = ax[1]
+
+fig_sims, ax_sims = plt.subplots(figsize=(5, 5))
+
+select = (sims_Om < 0.5)
+ax_sims.scatter(sims_Om[select], sims_S8[select], c=black, marker='x', s=3)
+
+im = ax_sims.scatter(true_Om[where], true_S8[where], c=counts, vmin=0)
+divider = make_axes_locatable(ax_sims)
+cax = divider.append_axes('right', size='5%', pad=0.05)
+cbar = plt.colorbar(im, cax=cax)
+cbar.set_label('number of catastrophic failures')
+ax_sims.set_xlabel('$\Omega_m$')
+ax_sims.set_ylabel('$S_8$')
+
+rect = plt.Rectangle( (prior['Om'][0], prior['S8'][0]),
+                     prior['Om'][1]-prior['Om'][0], prior['S8'][1]-prior['S8'][0],
+                     fill=False, edgecolor=black)
+ax_sims.add_patch(rect)
+
+savefig(fig_sims, 'catastrophes_sims')
+
 
 for idx in tqdm(indices) :
     
