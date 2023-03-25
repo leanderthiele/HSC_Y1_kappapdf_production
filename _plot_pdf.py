107359d4bdeea5270eab2ab98b6f40a703c933bb
Leander Thiele <lthiele@princeton.edu>
2023-03-25 10:15:13 -0400

    stuff from della

--- _plot_pdf.py
+++ _plot_pdf.py
@@ -24,17 +24,29 @@ data = Data()
 kappa_edges = np.linspace(-4, 4, num=20)
 kappa = 0.5 * (kappa_edges[1:] + kappa_edges[:-1])
 
-d_fid = np.mean(data.get_datavec('fiducial'), axis=0)
-print(d_fid.shape)
-d_hsc = data.get_datavec('real')
+all_fid = data.get_datavec('fiducial')
+d_fid = np.mean(all_fid, axis=0)
+d_hsc = data.get_datavec('real')[0]
+sigma = np.std(all_fid, axis=0)
 
-fig, ax = plt.subplots(figsize=(5, 5))
+# make it zero at the origin
+offset = d_fid[9]
+d_fid -= offset
+d_hsc -= offset
+
+gaussian = -0.5 * kappa**2
+
+fig, ax = plt.subplots(figsize=(5, 3))
 
 ax.plot(kappa, d_fid, label='fiducial simulations')
-ax.plot(kappa, d_hsc, label='HSC Y1',
+ax.errorbar(kappa, d_hsc, yerr=sigma, label='HSC Y1',
         linestyle='none', marker='o')
+ax.plot(kappa, gaussian, label='Gaussian',
+        color=black)
 
+ax.set_xlim(-4, 4)
 ax.set_xlabel('$\kappa / \sigma(\kappa)$')
 ax.set_ylabel('$\log {\sf PDF}(\kappa) + {\sf const}$')
+ax.legend(frameon=False, loc='lower center')
 
 savefig(fig, 'pdf')
